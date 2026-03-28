39b857c2a1ef71e5497281d5076845e9605852bf
Jazib Askari <jazibaskari@Mac.lan>
2026-03-28 00:33:57 +0000

    feat(home): add HomeSection component (#3)

---
+++ jazib-askari/src/components/home/HomeSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Section from "../shared/Section";
+
+const HomeSection: React.FC = () => (
+  <Section id="home">
+    <h1>Welcome</h1>
+    <p>This is my portfolio.</p>
+  </Section>
+);
+
+export default HomeSection;
