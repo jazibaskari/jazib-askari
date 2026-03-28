0c8fd9965643893d6d139bcf3bc9d1021fbb17bc
Jazib Askari <jazibaskari@Mac.lan>
2026-03-28 00:34:33 +0000

    feat(about): add AboutSection component (#4)

---
+++ jazib-askari/src/components/about/AboutSection.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Section from "../shared/Section";
+const AboutSection: React.FC = () => (
+  <Section id="about">
+    <h2>About Me</h2>
+    <p>I am a front-end developer transitioning to data analytics.</p>
+  </Section>
+);
+export default AboutSection;
