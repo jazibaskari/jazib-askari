b0cdcfd7c5a237faa94023a5f16817b3eac64370
Jazib Askari <jazibaskari@Mac.lan>
2026-04-01 01:59:17 +0100

    refactor(about): updating about section (#4)

--- jazib-askari/src/components/about/AboutSection.tsx
+++ jazib-askari/src/components/about/AboutSection.tsx
@@ -15,7 +15,7 @@ const AboutSection = ({ trigger }: AboutSectionProps) => (
     </TextAnimation>
     <TextAnimation duration={1} trigger={trigger}>
       <Typography variant="body1" sx={{ color: "#bfc0c0" }}>    
-        I am a product-centric Frontend Developer with over 4 years of experience leveraging TypeScript to build responsive, scalable React applications...
+      I am a product-centric Frontend Developer with over 4 years of experience leveraging TypeScript to build responsive, scalable React applications. Having engineered enterprise-scale applications for 300,000+ users at PwC, I specialise in translating UI/UX designs into high-performance code, while ensuring cross-browser compatibility and mobile-first responsiveness. I thrive in remote environments that value ownership, data-driven decisions, and maintaining high coding standards through collaborative code reviews. My strategic approach ensures every UI decision aligns with technical feasibility, user needs, and the business's bottom line.
       </Typography>
     </TextAnimation>
   </Section>
