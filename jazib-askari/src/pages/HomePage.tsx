01d4e9e1e5524702687870d93839e07f74091f00
Jazib Askari <jazibaskari@Jazibs-MacBook-Pro.local>
2026-05-19 17:58:04 +0100

    feat(ui): delete skills section

--- jazib-askari/src/pages/HomePage.tsx
+++ jazib-askari/src/pages/HomePage.tsx
@@ -1,7 +1,6 @@
 import HomeSection from "../components/home/HomeSection";
 import AboutSection from "../components/about/AboutSection";
 import ProjectsSection from "../components/projects/ProjectsSection";
-import SkillsSection from "../components/skills/SkillsSection";
 import ExperienceSection from "../components/experience/ExperienceSection";
 import DisclaimerSection from "../components/disclaimer/DisclaimerSection";
 interface HomePageProps {
@@ -11,10 +10,9 @@ const HomePage = ({ navTrigger }: HomePageProps) => (
   <>
     <HomeSection trigger={navTrigger} />
     <AboutSection trigger={navTrigger} />
-    <SkillsSection trigger={navTrigger} />
-    <ExperienceSection trigger={navTrigger}/>
+    <ExperienceSection trigger={navTrigger} />
     <ProjectsSection trigger={navTrigger} />
-    <DisclaimerSection  />
+    <DisclaimerSection />
   </>
 );
 export default HomePage;
