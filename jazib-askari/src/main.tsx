508b5dd7fdbf30df911162e157cfac38fbc50c47
Jazib Askari <jazibaskari@Mac.lan>
2026-05-22 16:13:42 +0100

    fix(ui): remove font import

--- jazib-askari/src/main.tsx
+++ jazib-askari/src/main.tsx
@@ -2,10 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/fonts.css";
-import "@fontsource/inter/300.css";
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/700.css";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
